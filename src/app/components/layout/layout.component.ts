48d5c4baa8b702e89c630eb2b657b49ddd8623f4
phpusr <phpusr@gmail.com>
2022-07-11 17:15:03 +0500

    Fixed import data link

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -2,9 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'
 import { Observable } from 'rxjs'
 import { map, shareReplay } from 'rxjs/operators'
-import { ImportDataDialog } from 'src/app/pages/import-data/import-data.component'
-import { MatDialog } from '@angular/material/dialog'
-import { ActivatedRoute, Router } from '@angular/router'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-layout',
@@ -19,7 +17,7 @@ export class LayoutComponent implements OnInit {
       shareReplay()
     )
 
-  constructor(private breakpointObserver: BreakpointObserver) { }
+  constructor(private breakpointObserver: BreakpointObserver, public router: Router) { }
 
   ngOnInit(): void {
   }
