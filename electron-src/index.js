af796968373b0b9155a09af010978a9e2803ab7f
phpusr <phpusr@gmail.com>
2023-02-02 00:25:39 +0500

    Added electron files

---
+++ electron-src/index.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('../dist/rent-store/index.html')
+
+  // Open the DevTools.
+  //win.webContents.openDevTools()
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
