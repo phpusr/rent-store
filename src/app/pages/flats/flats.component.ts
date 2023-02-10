defc82fe8c10ec926576049251f353a589b1b482
phpusr <phpusr@gmail.com>
2023-02-10 15:13:14 +0500

    Added content flats page content

--- src/app/pages/flats/flats.component.ts
+++ src/app/pages/flats/flats.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit, AfterViewInit, OnDestroy } from '@angular/core'
+import { Subscription } from 'rxjs'
+import { Flat } from 'src/app/interfaces/general'
+import { DataStorageService } from 'src/app/services/data_storage.service'
+import { MatSort } from '@angular/material/sort'
+import { MatTableDataSource } from '@angular/material/table'
 
 @Component({
   selector: 'app-flats',
   templateUrl: './flats.component.html',
   styleUrls: ['./flats.component.scss']
 })
-export class FlatsComponent implements OnInit {
+export class FlatsComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  constructor() { }
+  dataSource = new MatTableDataSource<Flat>()
+  displayColumns = ['id', 'address', 'number']
+  private flatsSub?: Subscription
+
+  @ViewChild(MatSort) sort!: MatSort
+
+  constructor(private dataStorage: DataStorageService) { }
 
   ngOnInit(): void {
+    this.flatsSub = this.dataStorage.flats$.subscribe(flats => {
+      this.dataSource = new MatTableDataSource(flats)
+    })
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort
+  }
+
+  ngOnDestroy(): void {
+    this.flatsSub?.unsubscribe()
   }
 
 }
